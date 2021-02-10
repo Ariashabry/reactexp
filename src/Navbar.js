a29083747aa85260b35bb23fe81ec489d1ae0692
shabry-nozyra <shabry@nozyra.com>
2021-02-10 16:45:42 +0700

    Front End With React

---
+++ src/Navbar.js
@@ -0,0 +1,56 @@
+// class component (untuk componen yang besar_)
+// rcc
+
+// import React, { Component } from 'react'
+
+// export default class Navbar extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Test drive</h3>
+//             </div>
+//         )
+//     }
+// }
+
+//function componen (rfc)
+
+// import React from 'react'
+
+// export default function Navbar() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+
+//menggunakan arrow function rfac
+//untuk yang kecil
+
+// import React from 'react'
+
+// const Navbar = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+// export default Navbar
+
+
+
+import React from 'react'
+export const Navbar = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+
+
+
